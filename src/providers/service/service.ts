5caca1cae662c0bb0ea687dfc11714d9bf56d29f
dyhayu <oktavianidyah30@gmail.com>
2020-06-22 08:43:04 +0700

    UasAppMobMulti

---
+++ src/providers/service/service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ServiceProvider {
+  API_KEY = 'https://newsapi.org/v2/top-headlines?country=id&apiKey=3ad71ea59e044bd4b6c06390bb7c3d33';
+
+  constructor(public httpClient: HttpClient) {
+    console.log('Hello ServiceProvider Provider');
+  }
+
+  getNews(){
+    return this.httpClient.get(this.API_KEY);
+  }
+
+}
+
+
+
